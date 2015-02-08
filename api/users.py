04097686c25cc2cfa76bad1254b0e2ad43d5613d
Mihir Singh <git.service@mihirsingh.com>
2015-02-08 13:04:08 -0600

    Created a new AuthenticatedResource class for endpoints that require authentication

--- api/users.py
+++ api/users.py
@@ -1,7 +1,33 @@
 from flask import request, abort, g
 from . import User, Token, Resource
 import bcrypt
-import os
+from functools import wraps
+
+def verify(function):
+
+    @wraps(function)
+    def wrapper(*args, **kwargs):
+
+        token = request.headers.get('X-Authentication-Token')
+
+        if token is None:
+
+            abort(401)
+
+        else:
+            try:
+                token = Token.get(Token.token == token)
+                g.user = token.user
+
+            except Token.DoesNotExist:
+
+                abort(401)
+        return function(*args, **kwargs)
+    return wrapper
+
+class AuthenticatedResource (Resource):
+
+    method_decorators = [verify]
 
 class AuthenticationResource(Resource):
 
